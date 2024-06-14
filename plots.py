1515013e556848aaa4c08813f99e4844dc660417
Max Nagy <maxmunzel@me.com>
2024-06-14 10:44:08 +0200

    FRIDAY

--- plots.py
+++ plots.py
@@ -14,7 +14,10 @@ def __():
 
 @app.cell
 def __(pd):
-    df = pd.read_csv("/Users/casparfriedrich/Downloads/wandb_export_2024-06-13T11_21_36.289+02_00.csv", index_col="simulation_steps")
+    df = pd.read_csv(
+        "/Users/casparfriedrich/Downloads/wandb_export_2024-06-13T11_21_36.289+02_00.csv",
+        index_col="simulation_steps",
+    )
     return df,
 
 
@@ -45,16 +48,32 @@ def __(df):
 
 @app.cell
 def __(df, np, plt):
-    fig, ax = plt.subplots(figsize=(6,5))
+    fig, ax = plt.subplots(figsize=(6, 5))
     for i, k in enumerate(df):
+        if "mocap" in k:
+            continue
         df[k].dropna().plot(ax=ax, linewidth=3)
     ax.legend(loc="upper right")
-    ax.set_xticks(1e7*np.arange(6), [f"{i}M" for i in range(0, 60, 10)])
+    ax.set_xticks(1e7 * np.arange(6), [f"{i}M" for i in range(0, 60, 10)])
     ax.set_xlabel("Simulation Steps")
     ax.set_ylabel("Evaluation Success Rate (Sim)")
-    ax.set_ylim(0,1)
+    ax.set_ylim(0, 1)
     ax.grid(True, axis="y")
     fig.savefig("media/clipping_reward.pdf")
+    fig
+
+    fig, ax = plt.subplots(figsize=(6, 5))
+    for i, k in enumerate(df):
+        if not "clipped" in k or "mocap" in k:
+            continue
+        df[k].dropna().plot(ax=ax, linewidth=3, label=k.replace("clipped", "")+" Episode")
+    ax.legend(loc="upper right")
+    ax.set_xticks(1e7 * np.arange(6), [f"{i}M" for i in range(0, 60, 10)])
+    ax.set_xlabel("Simulation Steps")
+    ax.set_ylabel("Evaluation Success Rate (Sim)")
+    ax.set_ylim(0, 1)
+    ax.grid(True, axis="y")
+    fig.savefig("media/training.pdf")
     fig
     return ax, fig, i, k
 
